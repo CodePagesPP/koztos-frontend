cb923062ec871f680499aa8505f7b5640beeb0a6
Espinozaje <jeisson12aaron@gmail.com>
2026-02-11 19:52:31 -0500

    responsive

--- src/app/components/layouts/nav/nav.component.ts
+++ src/app/components/layouts/nav/nav.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterModule} from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  isMenuOpen = window.innerWidth > 768;
   constructor(
     private authService: AuthService
   ) {}
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  handleNavLinkClick() {
+    if (window.innerWidth <= 768) {
+      this.closeMenu();
+    }
+  }
 
   logout(): void {
     this.authService.logout();
